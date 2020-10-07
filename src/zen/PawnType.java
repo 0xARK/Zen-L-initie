f45432912e5bedf339ea2d1f627694625e886a09
Matéo <67783173+0xARK@users.noreply.github.com>
2020-10-07 21:05:46 +0200

    Add files via upload

---
+++ src/zen/PawnType.java
@@ -0,0 +1,17 @@
+package zen;
+
+import java.io.Serializable;
+
+/**
+ * This class allows to declare the type of the pawn to find out wich player it belongs to
+ * @author Castella Mateo
+ * @version 1.0.0
+ */
+
+public enum PawnType implements Serializable {
+
+	ZEN,
+	WHITE,
+	BLACK;
+
+}
